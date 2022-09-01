ca887d523ffd879390901a66e82918b0c1ae83f5
LidiaJarmolkiewicz <lidiajarmolkiewicz@gmail.com>
2022-09-01 19:58:29 +0200

    popr

--- Zadanie6/Zadanie6.cpp
+++ Zadanie6/Zadanie6.cpp
@@ -52,11 +52,13 @@ int main()
 	std::cout << "lista liczb niepierwszych" << std::endl;
 
 	
+	auto NotInPrimList = [&listOfPrims](int i) {return  listOfPrims.end() ==  std::find(listOfPrims.begin(), listOfPrims.end(), i); };
 	
-	listOfNoPrims.erase(std::remove(listOfNoPrims.begin(), listOfNoPrims.end(),listOfPrims), listOfNoPrims.end());
+	listOfNoPrims.erase(std::remove_if(listOfNoPrims.begin(), listOfNoPrims.end(),NotInPrimList), listOfNoPrims.end());
 	for (const auto& e : listOfNoPrims)
 	{
 
 		std::cout << e << " ";
 	};
+	
 }
